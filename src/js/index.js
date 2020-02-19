b1e82eb463080c7992400a4c64cfae1fb5a0365d
Krzysztof Nyrek <krzysztof.nyrek@gmail.com>
2020-02-19 18:35:49 +0100

    Added search button functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,32 @@
 // Global app controller
 import Search from './modules/Search';
-const search = new Search('pizza');
+/* Global state of the app
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping list object
+ *  - Liked recipes
+ */
 
-search.getResult();
+const state = {};
+
+const controlSearch = async () => {
+  // Get query from view
+  const query = 'pizza';
+  
+  if (query){
+    //New search object and add to stage
+    state.search = new Search(query);
+    // Prepare UI for result
+
+    //Search for recipes
+    await state.search.getResult();
+
+    //Render result on UI
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector('.search').addEventListener('submit', e =>{
+  e.preventDefault();
+  controlSearch();
+});
